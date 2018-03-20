98e5bb9596ee08e360cd2ddaf79e98dec826923c
石垣駿 <ishigakishun@ishigakishun-no-MacBook-Pro.local>
2018-03-20 18:00:22 +0900

    Viewのフローティング

---
+++ app/src/main/java/com/example/s_ishigaki/floatingscreen/FloatingConfig.kt
@@ -0,0 +1,3 @@
+package com.example.s_ishigaki.floatingscreen
+
+class FloatingConfig (var floatingText: String)
--- app/src/main/java/com/example/s_ishigaki/floatingscreen/FloatingService.kt
+++ app/src/main/java/com/example/s_ishigaki/floatingscreen/FloatingService.kt
@@ -8,15 +8,37 @@ import android.os.IBinder
 import android.app.NotificationManager
 import android.app.NotificationChannel
 import android.content.Context
+import android.graphics.PixelFormat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
 
 class FloatingService: Service() {
+
+    private lateinit var view: View
+    private lateinit var windowManager: WindowManager
 
     override fun onBind(p0: Intent?): IBinder? = null
 
     override fun onCreate() {
         super.onCreate()
+    }
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // Service起動中の通知を表示
         startNotification()
+
+        // Viewをフローティングさせる
+        startFloating()
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // フローティングしているViewを削除
+        windowManager.removeView(view)
     }
 
     private fun startNotification() {
@@ -37,13 +59,33 @@ class FloatingService: Service() {
         manager.createNotificationChannel(channel)
 
         val notification = Notification.Builder(this, "channel_1")
-                .setContentTitle("通知タイトル")
-                .setContentText("通知コンテンツ")
+                .setContentTitle("Floating App")
+                .setContentText("フローティングサービス起動中")
                 .setContentIntent(pendingIntent)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .build()
 
         // フローティング画面を表示するためにServiceをフォアグラウンド化
         startForeground(1, notification)
+    }
+
+    private fun startFloating() {
+        // WindowManagerを初期化
+        windowManager = applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+        val layoutInflater = LayoutInflater.from(this)
+        val layoutParams = WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                PixelFormat.TRANSLUCENT
+        )
+
+        // フローティングさせるViewを生成
+        view = layoutInflater.inflate(R.layout.floating_service, null)
+
+        // フローティング画面にViewを追加
+        windowManager.addView(view, layoutParams)
     }
 }
