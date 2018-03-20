86c08ae5fc847ba862a29629a65ede7ee61dab3d
石垣駿 <ishigakishun@ishigakishun-no-MacBook-Pro.local>
2018-03-20 16:31:41 +0900

    Notificationをバージョン8に変更

--- app/src/main/java/com/example/s_ishigaki/floatingscreen/FloatingService.kt
+++ app/src/main/java/com/example/s_ishigaki/floatingscreen/FloatingService.kt
@@ -5,6 +5,9 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.app.NotificationManager
+import android.app.NotificationChannel
+import android.content.Context
 
 class FloatingService: Service() {
 
@@ -19,15 +22,28 @@ class FloatingService: Service() {
     private fun startNotification() {
         val intent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-        val notification = Notification.Builder(this)
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val channel = NotificationChannel(
+                // アプリでユニークな ID
+                "channel_1",
+                // ユーザが「設定」アプリで見ることになるチャンネル名
+                "channel_1",
+                // チャンネルの重要度
+                NotificationManager.IMPORTANCE_DEFAULT
+        )
+
+        // 端末にチャンネルを登録し、「設定」で見れるようにする
+        manager.createNotificationChannel(channel)
+
+        val notification = Notification.Builder(this, "channel_1")
+                .setContentTitle("通知タイトル")
+                .setContentText("通知コンテンツ")
                 .setContentIntent(pendingIntent)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("フローティングAPP")
-                .setContentText("画面が浮いてるよ")
                 .build()
 
         // フローティング画面を表示するためにServiceをフォアグラウンド化
         startForeground(1, notification)
     }
-
 }
